dc22d48ed8599b1124c3285b5a4c3b44b4ff2280
pjho33 <pjho333@gmail.com>
2025-12-11 08:59:28 +0900

    Final cleanup: Track only scripts folder and necessary configs.

--- run_pipeline_now.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="/home/pjho3/miniconda3/bin:$PATH"
-source /home/pjho3/miniconda3/etc/profile.d/conda.sh
-cd /home/pjho3/projects/Drug
-SMILES=$(cat tripod_peg6_l_glucose.smi)
-./run_manual_pipeline.sh raw_data/4PYP_trimer.pdb "$SMILES" run_tripod_peg6_final
